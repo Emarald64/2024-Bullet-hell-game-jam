c798de3df4a9a9e60e339bf3d644b29d6e2b3968
Alex Giller <alexgiller33@gmail.com>
2024-05-11 12:02:44 -0400

    added health bar above player

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -1,15 +1,15 @@
 extends Area2D
-signal hit
 signal dash
-signal heal
 
 @export var AccelerationDivider=3
 @export var SpeedMultipier=800
 @export var Friction=1.5
-var speed=400
+var speed=400.0
 var screen_size
 var play_size
 var dead
+var health=5
+var maxHealth=5
 var dashing=false
 var velocity=Vector2.ZERO
 var dashspeed
@@ -42,8 +42,9 @@ func _process(delta):
 			invincible = true
 			if velocity.length()<speed/2:
 				velocity*=speed/2/velocity.length()
-			
-			
+		if Input.is_action_just_pressed("debug_hit"):
+			health-=1
+			update_health_bar()
 
 	if dead!=true:
 		#if velocity.x>2:velocity.x=2
@@ -56,22 +57,23 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.get_meta('OnHit')=='damage' and not invincible:
-		hit.emit()
+		health-=1
+		update_health_bar()
 		$InvincibleTimer.start()
 		self.modulate=Color(1.0,1.0,1.0,0.5)
 		$HitSound.play()
 		# Must be deferred as we can't change physics properties on a physics callback.
 		invincible=true
 #const powers=['health','dash','speed']
-	elif body.get_meta('OnHit')=='power':
-		if body.power=='health':emit_signal('heal')
-		elif body.power=='dash':
-			dashmod+=1
-			$DashCooldown.wait_time=(dashmod+1)/(2*dashmod+1)
-		elif body.power=='speed':
-			speedmod+=1
-			speed=((-1.0/(speedmod+3))+1)*SpeedMultipier
-		body.queue_free()
+	#elif body.get_meta('OnHit')=='power':
+		#if body.power=='health':emit_signal('heal')
+		#elif body.power=='dash':
+			#dashmod+=1
+			#$DashCooldown.wait_time=(dashmod+1)/(2*dashmod+1)
+		#elif body.power=='speed':
+			#speedmod+=1
+			#speed=((-1.0/(speedmod+3))+1)*SpeedMultipier
+		#body.queue_free()
 	
 func start(pos):
 	position = pos
@@ -92,3 +94,12 @@ func _on_dash_end():
 	$DashCooldown.start()
 	if $InvincibleTimer.is_stopped():invincible = false
 	
+func update_health_bar():
+	$HealthBar.value=health
+	$HealthBar.max_value=maxHealth
+	if health==1:
+		$HealthBar.tint_progress=Color(1,0,0)
+	elif health<=maxHealth/2.0:
+		$HealthBar.tint_progress=Color(1,1,0)
+	else:
+		$HealthBar.tint_progress=Color(0,1,0)
