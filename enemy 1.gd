50a8a183d18cfe0271ac834d5221a3828c2d1fc6
Alex Giller <alexgiller33@gmail.com>
2024-05-11 13:02:23 -0400

    added first enemy

---
+++ enemy 1.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var moveSpeed=150
+var screen_size
+@export var bulletScene: PackedScene
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x+=moveSpeed*delta
+	if position.x>screen_size.x:moveSpeed*=-1
+	elif position.x<0:moveSpeed*=-1
+
+
+func _on_bullet_timer_timeout():
+	var bullet = bulletScene.instantiate()
+	bullet.position=position
+	add_child(bullet)
