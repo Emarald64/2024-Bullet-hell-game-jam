515dfe1879d421013ddd561f739438966f54348d
Alex Giller <alexgiller33@gmail.com>
2024-05-11 19:17:39 -0400

    actually comiited changes

--- assets/scripts/enemy 1.gd
+++ assets/scripts/enemy 1.gd
@@ -4,19 +4,20 @@ var moveSpeed=150
 var bulletSpeed=200
 var screen_size
 var dieing=false
+var spawning=true
 @export var bulletScene: PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	await get_tree().create_timer(randf_range(1,2)).timeout
+	$BulletTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not dieing:
 		position.x+=moveSpeed*delta
-		if position.x>screen_size.x:moveSpeed*=-1
-		elif position.x<0:moveSpeed*=-1
-
+		if (position.x>screen_size.x or position.x<0) and not spawning:moveSpeed*=-1
+		if position.x<screen_size.x and position.x>0:spawning=false
 
 func _on_bullet_timer_timeout():
 	if not dieing:
--- assets/scripts/main.gd
+++ assets/scripts/main.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@export var enemy1:PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("debug_spawn_enemy1"):spawn_enemy1()
+
+func spawn_enemy1():
+	var enemy=enemy1.instantiate()
+	enemy.position=Vector2(-16,0)
+	add_child(enemy)
